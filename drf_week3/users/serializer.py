65aaa358b0980c6ddbe311b3255cab030627f37f
Seman_Kim <sdoramkim@gmail.com>
2023-04-26 15:16:53 +0900

    Update jwt_informaiton
    login 시도시 jwt토큰에 email 정보 포함하기
    serializer.py 사용할 serailizer 작성
    views.py serializer import후 사용
    usls.py 사용할 views변경 하기

--- drf_week3/users/serializer.py
+++ drf_week3/users/serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from users.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,3 +21,11 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+    
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
--- drf_week3/users/views.py
+++ drf_week3/users/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-
-from users.serializer import UserSerializer
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from users.serializer import CustomTokenObtainPairSerializer, UserSerializer
 
 class UserView(APIView):
     def post(self, request):
@@ -12,3 +15,6 @@ class UserView(APIView):
             return Response({"message":"가입완료!"}, status=status.HTTP_201_CREATED)
         else:
             return Response({"message":f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+        
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
