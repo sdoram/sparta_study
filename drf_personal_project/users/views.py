6aff09b49ca9978f6ae15601ccda2131b29d8a8a
Seman_Kim <sdoramkim@gmail.com>
2023-04-29 18:35:19 +0900

    Create feature/users_put
    유저 정보 수정 가능, email 비밀번호(비밀번호는 해시 사라짐)까지 바꿀 수 있는 문제 존재
    email은 불변, 비밀번호 변경시 다시 해싱해서 데이터 베이스 입력까지
    수정해야함

--- drf_personal_project/users/views.py
+++ drf_personal_project/users/views.py
@@ -21,12 +21,29 @@ class UserView(APIView):
 
 class UserDetailView(APIView):
     def get(self, request, id):
+        # 검색 id로 User정보 검색 
         user = get_object_or_404(User, id=id)
+        # 현재 user와 검색 user 일치 판단
         if request.user == user:
             serializer = UserDetailSerializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({"message":"본인이 아닙니다"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+    def put(self, request, id):
+        # 회원정보 수정 PUT
+        user = get_object_or_404(User, id=id)
+        serializer = UserDetailSerializer(user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        # if request.user == user:
+        # else:
+            # return Response({"message":"본인이 아닙니다"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    def delete(self, request, id):
+        # 회원 탈퇴 DELETE
+        pass
 
 
 
@@ -42,7 +59,7 @@ class mockView(APIView):
         return Response("get 요청")
 
 
-# 로그인 POST
-# 로그아웃 POST
-# 회원정보 수정 PUT
-# 회원 탈퇴 DELETE
+# 로그인 POST <- users/api/token에서 로그인
+# 로그아웃 POST <- token으로 로그인 유효 시간이 있는데 로그아웃 따로 되나? <- token 방식은 백엔드에서 일단 구현 안하고 넘어감
+
+
