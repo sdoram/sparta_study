fe6a891c5f3580e5fdf78bf7cffa3c64eea8d941
Seman_Kim <sdoramkim@gmail.com>
2023-04-29 20:07:34 +0900

    Update featrue/users_put
    serializers.py 유저디테일 email 수정 불가, password 확인 불가 추가
    views.py UserDetailView put메소드 비밀번호 해싱 추가

--- drf_personal_project/users/serializers.py
+++ drf_personal_project/users/serializers.py
@@ -27,7 +27,11 @@ class UserSerializer(serializers.ModelSerializer):
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ["email","password","name","gender","age","introduction"]
+        extra_kwargs = {
+            "password": {'write_only': True},
+            "email":{'read_only' : True},
+        }
     
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -38,9 +42,8 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         # Add custom claims
         token['email'] = user.email
         token['name'] = user.name
-        token['age'] = user.age
         token['gender'] = user.gender
-        token['introduction'] = user.introduction
+        token['age'] = user.age
         # ...
 
         return token
--- drf_personal_project/users/views.py
+++ drf_personal_project/users/views.py
@@ -20,28 +20,26 @@ class UserView(APIView):
         serializer.save()
         return Response({"message":"가입완료!"}, status=status.HTTP_201_CREATED)
 
-class UserDetailView(APIView):
 
+class UserDetailView(APIView):
     def get(self, request,id):
-        # 검색 id로 User정보 검색 
         user = get_object_or_404(User, id=id)
-        # 현재 user와 검색 user 일치 판단
-        if request.user == user:
-            serializer = UserDetailSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response({"message":"본인이 아닙니다"}, status=status.HTTP_401_UNAUTHORIZED)
-        
+        serializer = UserDetailSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def put(self, request, id):
         # 회원정보 수정 PUT
         user = get_object_or_404(User, id=id)
-        serializer = UserDetailSerializer(user, data=request.data)
-        # if request.user == user:
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-        # else:
-            # return Response({"message":"본인이 아닙니다"}, status=status.HTTP_401_UNAUTHORIZED)
+        serializer = UserDetailSerializer(user, data=request.data,partial=True)
+        # 현재 user와 검색 user 일치 판단
+        if request.user == user:
+            serializer.is_valid(raise_exception=True)
+            password = request.data.get("password")
+            user.set_password(password)
+            user.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"message":"본인이 아닙니다"}, status=status.HTTP_401_UNAUTHORIZED)
 
     def delete(self, request, id):
         # 회원 탈퇴 DELETE
@@ -53,11 +51,10 @@ class UserDetailView(APIView):
             return Response({"message":"본인이 아닙니다"}, status=status.HTTP_401_UNAUTHORIZED)
 
 
-
-
 class CustomTokenObtainPairView(TokenObtainPairView):
     # 최신 버전의 docs에는 없는데 무슨 차이가 있는지 알아보기 
     serializer_class = CustomTokenObtainPairSerializer
+
 
 class mockView(APIView):
     permission_classes = [permissions.IsAuthenticated]
